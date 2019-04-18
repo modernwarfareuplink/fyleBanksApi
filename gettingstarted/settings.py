39986d98606fb0dfcc179008d68f2eeeef74f533
arun tvs <aruntvs227@gmail.com>
2019-04-18 14:30:05 +0530

    update database settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
