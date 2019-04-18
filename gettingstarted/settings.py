40aeabcf74c957fec334930b4d59460dce4f6115
arun tvs <aruntvs227@gmail.com>
2019-04-18 14:22:39 +0530

    updates

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fyledb',
+        'USER': 'postgres',
+        'PASSWORD': 'passowrd', # replace your password here
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
